d4b6bb72bf153d1b28cad76914a4259ba4d4d92f
liusaifei2018012702 <1465422485@qq.com>
2023-10-10 20:16:58 +0900

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+   这个也行
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    我只想要这一个
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
